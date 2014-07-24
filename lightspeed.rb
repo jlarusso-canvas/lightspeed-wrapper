41237a2bfd54bcc4d7f0e707121b2ccbfad0fe32
Jesse La Russo <jlarusso@canvas.is>
2014-07-24 10:38:05 -0400

    Add new sample calls

--- lightspeed.rb
+++ lightspeed.rb
@@ -23,14 +23,26 @@ class Lightspeed
     end
   end
 
+  def req_controls
+    @client.start do |http|
+      req = net_get("/API/Controls.json")
+      req.basic_auth(@key, 'apikey')
+      http.request(req)
+    end
+  end
+
   def net_get(url)
     Net::HTTP::Get.new(url)
   end
 end
 
 l = Lightspeed.new
+puts 'see your permissions:  JSON.parse l.req_controls.body'
 puts 'try:  l.fetch("Category.json")'
 puts 'try:  l.fetch("Item.json")'
 puts 'try:  l.fetch("Item/2.json")'
-
+puts 'try:  l.fetch "Image.json"'
+puts 'try:  l.fetch "Item.json?tax=true"'
+puts 'try:  l.fetch "InventoryCountItem.json?itemID=6"'
+puts 'try:  l.fetch "Item/6.json?load_relations=all"'
 binding.pry
