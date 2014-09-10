4c7af2001b0cdacfa1144bcd99ceb8581b7d06aa
Jesse La Russo <jlarusso@canvas.is>
2014-09-10 10:28:43 -0400

    Add ability to delete

--- lightspeed.rb
+++ lightspeed.rb
@@ -11,21 +11,22 @@ class Lightspeed
     @client.use_ssl = true
   end
 
-  def fetch(url)
-    JSON.parse make_request(url).body
+  def get(url)
+    parse(url, 'get')
   end
 
-  def make_request(url)
-    @client.start do |http|
-      req = net_get("/API/Account/#{@id}/#{url}")
-      req.basic_auth(@key, 'apikey')
-      http.request(req)
-    end
+  def delete(url)
+    parse(url, 'delete')
   end
 
-  def req_controls
+  def parse(url, method)
+    JSON.parse make_request(url, method).body
+  end
+
+  def make_request(url, method)
+    method_name = "net_#{method}"
     @client.start do |http|
-      req = net_get("/API/Controls.json")
+      req = send(method_name, "/API/Account/#{@id}/#{url}")
       req.basic_auth(@key, 'apikey')
       http.request(req)
     end
@@ -34,15 +35,20 @@ class Lightspeed
   def net_get(url)
     Net::HTTP::Get.new(url)
   end
+
+  def net_delete(url)
+    Net::HTTP::Delete.new(url)
+  end
 end
 
 l = Lightspeed.new
 puts 'see your permissions:  JSON.parse l.req_controls.body'
-puts 'try:  l.fetch "Category.json"'
-puts 'try:  l.fetch "Item.json"'
-puts 'try:  l.fetch "Item/2.json"'
-puts 'try:  l.fetch "Image.json"'
-puts 'try:  l.fetch "Item.json?tax=true"'
-puts 'try:  l.fetch "InventoryCountItem.json?itemID=6"'
-puts 'try:  l.fetch "Item/6.json?load_relations=all"'
+puts 'try:  l.get "Category.json"'
+puts 'try:  l.get "Item.json"'
+puts 'try:  l.get "Item/2.json"'
+puts 'try:  l.get "Image.json"'
+puts 'try:  l.get "Item.json?tax=true"'
+puts 'try:  l.get "InventoryCountItem.json?itemID=6"'
+puts 'try:  l.get "Item/6.json?load_relations=all"'
+puts 'try:  l.delete "Item/6.json"'
 binding.pry
