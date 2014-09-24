3a4aaa1eb27151666a176e4dc50a96d884e19475
Jesse La Russo <jlarusso@canvas.is>
2014-09-24 12:28:32 -0400

    Set up 'post'ing

--- lightspeed.rb
+++ lightspeed.rb
@@ -19,27 +19,51 @@ class Lightspeed
     parse(url, 'delete')
   end
 
-  def parse(url, method)
-    JSON.parse make_request(url, method).body
+  def post(url, params)
+    parse(url, 'post', params)
   end
 
-  def make_request(url, method)
+  def parse(url, method, params = nil)
+    JSON.parse make_request(url, method, params).body
+  end
+
+  def make_request(url, method, params = nil)
     method_name = "net_#{method}"
     @client.start do |http|
-      req = send(method_name, "/API/Account/#{@id}/#{url}")
+      req = send(method_name, "/API/Account/#{@id}/#{url}", params)
       req.basic_auth(@key, 'apikey')
       http.request(req)
     end
   end
 
-  def net_get(url)
+  def net_get(url, params)
     Net::HTTP::Get.new(url)
   end
 
-  def net_delete(url)
+  def net_delete(url, params)
     Net::HTTP::Delete.new(url)
   end
+
+  def net_post(url, params)
+    puts params
+    req = Net::HTTP::Post.new(url, initheader = {'Content-Type' => 'application/json'})
+    req.body = params.to_json
+    req
+  end
 end
+
+sale = {}
+sale["completed"] = true
+sale["Shop"] = {}
+sale["Shop"]["shopID"] = "11"
+sale["SaleLines"] = {}
+sale["SaleLines"] = []
+
+sale_line = {}
+sale_line["itemID"] = "929"
+sale_line["unitQuantity"] = "1"
+
+sale["SaleLines"] << { "SaleLine" => sale_line }
 
 l = Lightspeed.new
 puts 'see your permissions:  JSON.parse l.req_controls.body'
@@ -52,4 +76,5 @@ puts 'try:  l.get "InventoryCountItem.json?itemID=6"'
 puts 'try:  l.get "Item/6.json?load_relations=all"'
 puts 'try:  l.delete "Item/6.json"'
 puts 'try:  l.get "Item.json?limit=999"'
+puts 'try:  l.post "Sale.json", sale'
 binding.pry
